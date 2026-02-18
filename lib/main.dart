bedba34cd7d574e1cbc9520b4a4d8588d3fd9ccc
RachmanTri <rachman.23180@mhs.unesa.ac.id>
2026-02-18 16:16:23 +0700

    finalized prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:camera/camera.dart';
 import 'dart:io';
 // import 'dart:typed_data';
@@ -35,6 +36,10 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
 
   bool _isProcessing = false;
   String _faceCodeText = "Arahkan Wajah Ke Kamera";
+
+  List<Map<String, dynamic>> _registeredFaces = [];
+
+  Rect? _faceRect;
 
   @override
   void initState() {
@@ -101,14 +106,40 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
 
         if (faces.isNotEmpty) {
           final face = faces.first;
-
           final bytes = await File(inputImage.filePath!).readAsBytes();
           final img = imeg.decodeImage(bytes);
 
-          if (img == null) {
-            print("Gagal Decode Image");
-            return;
-          }
+          Rect scaledReact;
+
+          Size imageSize = Size(img!.width.toDouble(), img.height.toDouble());
+          Size previewSize = _cameraController!.value.previewSize!;
+
+          double scaleX = previewSize.width / imageSize.width;
+          double scaleY = previewSize.height / imageSize.height;
+
+          final double mirrorLeft = imageSize.width - face.boundingBox.right;
+          final double mirrorRight = imageSize.width - face.boundingBox.left;
+
+          scaledReact = Rect.fromLTRB(
+            mirrorLeft * scaleX,
+            face.boundingBox.top * scaleY,
+            mirrorRight * scaleX,
+            face.boundingBox.bottom * scaleY,
+          );
+
+          // double scaleX = previewSize.width / imageSize.width;
+          // double scaleY = previewSize.height / imageSize.height;
+
+          // final scaledReact = Rect.fromLTRB(
+          //   face.boundingBox.left * scaleX,
+          //   face.boundingBox.top * scaleY,
+          //   face.boundingBox.right * scaleX,
+          //   face.boundingBox.bottom * scaleY,
+          // );
+
+          setState(() {
+            _faceRect = scaledReact;
+          });
 
           final faceCrop = imeg.copyCrop(
             img,
@@ -118,6 +149,10 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
             height: face.boundingBox.height.toInt(),
           );
 
+          setState(() {
+            _faceRect = scaledReact;
+          });
+
           final resized = imeg.copyResize(faceCrop, width: 112, height: 112);
 
           var input = List.generate(1 * 112 * 112 * 3, (i) => 0.0);
@@ -125,22 +160,54 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
           for (int y = 0; y < 112; y++) {
             for (int x = 0; x < 112; x++) {
               final pixel = resized.getPixel(x, y);
-              int r = (pixel >> 16) & 0xFF;
-              int g = (pixel >> 8) & 0xFF;
-              int b = pixel & 0xFF;
+              int r = pixel.r.toInt();
+              int g = pixel.g.toInt();
 
+              int b = pixel.b.toInt();
+
+              input[pixelIndex++] = (r - 128) / 128.0;
+              input[pixelIndex++] = (g - 128) / 128.0;
+              input[pixelIndex++] = (b - 128) / 128.0;
             }
           }
 
-          var output = List.filled(128, 0.0).reshape([1, 128]);
+          var output = List.filled(192, 0.0).reshape([1, 192]);
           _interpreter!.run(input.reshape([1, 112, 112, 3]), output);
 
-          List<double> embedding = output[0];
+          List<double> embedding = List<double>.from(output[0]);
 
-          setState(() {
-            _faceCodeText =
-                "Wajah terdeteksi!\nEmbedding; ${embedding.take(5).toList()}....";
-          });
+          String? matchedName;
+          double maxSimilarity = 0.0;
+          const double threshold = 0.75;
+
+          for (var entry in _registeredFaces) {
+            double sim = cosineSimilarity(embedding, entry['embedding']);
+            if (sim > maxSimilarity && sim > threshold) {
+              maxSimilarity = sim;
+              matchedName = entry['name'];
+            }
+          }
+
+          if (matchedName != null) {
+            setState(() {
+              _faceCodeText = "Wajah Dikenali sebagai : $matchedName";
+            });
+          } else {
+            String? name = await _showNameInput(context);
+            if (name != null && name.isNotEmpty) {
+              _registeredFaces.add({'name': name, 'embedding': embedding});
+
+              setState(() {
+                _faceCodeText = "Wajah $name berhasil disimpan";
+              });
+            } else {
+              setState(() {
+                _faceCodeText = "nama dan embedding tidak dimasukkan";
+              });
+            }
+          }
+
+          // await _cameraController!.pausePreview();
         } else {
           setState(() {
             _faceCodeText = "Arahkan Wajah Ke Kamera";
@@ -152,6 +219,47 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
         _isCapturing = false;
       }
     }
+  }
+
+  double cosineSimilarity(List<double> a, List<double> b) {
+    double dot = 0.0;
+    double normA = 0.0;
+    double normB = 0.0;
+
+    for (int i = 0; i < a.length; i++) {
+      dot += a[i] * b[i];
+      normA += a[i] * a[i];
+      normB += b[i] * b[i];
+    }
+
+    return dot / (sqrt(normA) * sqrt(normB));
+  }
+
+  Future<String?> _showNameInput(BuildContext context) async {
+    TextEditingController _nameController = TextEditingController();
+
+    return showDialog<String>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Masukkan Nama"),
+          content: TextField(
+            controller: _nameController,
+            decoration: InputDecoration(hintText: "Contoh : Bambang"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, null),
+              child: Text("Batal"),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, _nameController.text),
+              child: Text("Simpan"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -172,16 +280,31 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
       body: Stack(
         children: [
           SizedBox(child: CameraPreview(_cameraController!)),
-          Center(
-            child: Container(
-              width: 250,
-              height: 250,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.greenAccent, width: 3),
-                borderRadius: BorderRadius.circular(16),
+
+          if (_faceRect != null)
+            Positioned(
+              left: _faceRect!.left + 128,
+              top: _faceRect!.top + 256,
+              width: _faceRect!.width,
+              height: _faceRect!.height,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green, width: 3),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            )
+          else
+            Center(
+              child: Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.greenAccent, width: 3),
+                  borderRadius: BorderRadius.circular(16),
+                ),
               ),
             ),
-          ),
 
           Positioned(
             bottom: 40,
@@ -193,18 +316,35 @@ class _FaceScanScreenState extends State<FaceScanScreen> {
                 color: Colors.black.withAlpha(200),
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: Text(
-                _faceCodeText,
-                style: TextStyle(color: Colors.white, fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
+              child: Column(
+                children: [
+                  Text(
+                    _faceCodeText,
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
 
-              // FloatingActionButton(
-              //   onPressed: () async {
-              //     _cameraController;
-              //   },
-              //   child: Icon(Icons.camera_alt),
-              // ),
+                  // ElevatedButton(
+                  //   onPressed: () async {
+                  //     setState(() {
+                  //       _isDetection = true;
+                  //       _faceCodeText = "Mendeteksi Ulang....";
+                  //     });
+                  //     await _cameraController?.resumePreview();
+                  //   },
+                  //   child: Text("Deteksi Ulang"),
+                  // ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Daftar wajah terdaftar:");
+                      for (var face in _registeredFaces) {
+                        print("- ${face['name']} - ${face['embedding']}");
+                      }
+                    },
+                    child: Text("Lihat Daftar Wajah"),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
